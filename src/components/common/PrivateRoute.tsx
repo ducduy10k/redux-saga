e814e01fa20cd72702023062e313483367a06f8b
rikkei-duynd <duynd@rikkeisoft.com>
2022-06-07 14:33:33 +0700

    21-6:00

--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { Navigate, Route } from 'react-router-dom';
 
 export interface IPrivateRouteProps {
-  element: React.ReactNode | null;
+  element: JSX.Element;
   [key: string]: any;
 }
 
 export function PrivateRoute(props: IPrivateRouteProps) {
   const isLoginIn = Boolean(localStorage.getItem('access_token'));
   if (!isLoginIn) return <Navigate to="/login"></Navigate>;
-  return <Route {...props} />;
+  return props.element;
 }
--- src/components/layout/Admin.tsx
+++ src/components/layout/Admin.tsx
@@ -1,7 +1,45 @@
+import { Box, createTheme } from '@mui/material';
 import * as React from 'react';
 
 export interface IAdminLayoutProps {}
 
 export function AdminLayout(props: IAdminLayoutProps) {
-  return <div>AdminLayout</div>;
+  const theme = createTheme();
+
+  return (
+    <Box
+      className="grid"
+      style={{
+        gridTemplateRows: 'auto 1fr',
+        gridAutoColumns: '300px 1fr',
+        gridTemplateAreas: `"header header" "sidebar main"`,
+        minHeight: '100vh',
+      }}
+    >
+      <Box
+        style={{
+          gridArea: 'header',
+          borderBottom: `1px solid ${theme.palette.divider}`,
+        }}
+        className="h-[60px] md:h-[65px]"
+      >
+        Header
+      </Box>
+      <Box
+        style={{
+          gridArea: 'sidebar',
+          borderRight: `1px solid ${theme.palette.divider}`,
+        }}
+      >
+        Sidebar
+      </Box>
+      <Box
+        style={{
+          gridArea: 'main',
+        }}
+      >
+        Main
+      </Box>
+    </Box>
+  );
 }
